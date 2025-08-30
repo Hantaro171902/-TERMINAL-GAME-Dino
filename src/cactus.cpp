e91d1602089bca51f32de3d77842260557dbbc2e
Hantaro171902 <chaugiahan001@gmail.com>
2025-08-30 14:46:05 +0700

    add cactus

--- src/cactus.cpp
+++ src/cactus.cpp
@@ -1,16 +1,365 @@
 #include "cactus.hpp"
+#include "game.hpp"
+#include <cstdint>
+#include <ctime>
+#include <random>
 #include <ncurses.h>
 
-void Cactus::draw(WINDOW* wnd) {
-    // Draw the cactus on the window
-    for (size_t i = 0; i < disp.size(); i++) {
-        mvaddch(wnd, pos[i].y, pos[i].x, disp[i]);
-    }
+using namespace std;
+
+Cactus::Cactus() : m_rd(), m_gen(m_rd()) {}
+
+rect Cactus::setBounds(rect a) {
+    m_field_bounds = a;
 }
 
-void Cactus::update(WINDOW* wnd) {
-    for (auto& p : pos) {
-        p.x--;
+void Cactus::getBounds() {
+    return m_field_bounds;
+}
+
+void Cactus::update(WINDOW* wnd, rect player_rect, int player_score) {
+    int dist_index = rand() % 2;
+
+  /* Update existing objects */
+  for (auto current_point{m_object_set.begin()};
+       current_point != m_object_set.end(); ++current_point) {
+
+    auto &current_object = *current_point;
+
+    if (current_object.getLastPos().x < m_field_bounds.left()) {
+      m_object_set.erase(current_point);
     }
-    update(wnd);
+
+    if (current_object.getFielded() || current_point == m_object_set.begin()) {
+      current_object.update(game_wnd);
+
+      vector<vec2i> obj_pos = current_object.getPos();
+      size_t length = current_object.getDispChar().size();
+      rect object_rect = {{obj_pos[0].x, obj_pos[0].y},
+                          {obj_pos[length - 1].x, obj_pos[length - 1].y}};
+
+      if (isCollided(player_rect, object_rect)) {
+        return true;
+      }
+    } else {
+      auto previous_object = *(std::prev(current_point));
+
+      int diff =
+          abs(previous_object.getLastPos().x - current_object.getLastPos().x);
+      if (dist_index == 0) {
+        if (diff >= 60) {
+          current_object.update(game_wnd);
+        }
+      }
+    }
+  }
+
+  if (m_object_set.size() < 7) {
+    int i = player_score > 500 ? 6 : 4;
+
+    /* Spawn a new object */
+    uniform_int_distribution<> distr(0, i);
+
+    /* generate random number between 0 and 6 */
+    auto object_type = distr(m_gen);
+    rect fb = getBounds();
+
+    string dc; /* Display characters */
+
+    int y_start;
+
+    switch (object_type) {
+    case 0: {
+      y_start = 16;
+      vector<vec2i> p{
+          {fb.bounds.x - 6, y_start},     {fb.bounds.x - 5, y_start},
+          {fb.bounds.x - 4, y_start},     {fb.bounds.x - 3, y_start},
+          {fb.bounds.x - 2, y_start},     {fb.bounds.x - 1, y_start},
+          {fb.bounds.x, y_start},         {fb.bounds.x - 6, y_start + 1},
+          {fb.bounds.x - 5, y_start + 1}, {fb.bounds.x - 4, y_start + 1},
+          {fb.bounds.x - 3, y_start + 1}, {fb.bounds.x - 2, y_start + 1},
+          {fb.bounds.x - 1, y_start + 1}, {fb.bounds.x, y_start + 1},
+          {fb.bounds.x - 6, y_start + 2}, {fb.bounds.x - 5, y_start + 2},
+          {fb.bounds.x - 4, y_start + 2}, {fb.bounds.x - 3, y_start + 2},
+          {fb.bounds.x - 2, y_start + 2}, {fb.bounds.x - 1, y_start + 2},
+          {fb.bounds.x, y_start + 2},     {fb.bounds.x - 6, y_start + 3},
+          {fb.bounds.x - 5, y_start + 3}, {fb.bounds.x - 4, y_start + 3},
+          {fb.bounds.x - 3, y_start + 3}, {fb.bounds.x - 2, y_start + 3},
+          {fb.bounds.x - 1, y_start + 3}, {fb.bounds.x, y_start + 3}};
+
+      dc = "   _   "
+           "  | |  "
+           " _| |_ "
+           "|_____|";
+
+      Cactus obj0{p, dc, 0};
+      m_object_set.push_back(obj0);
+      break;
+    }
+
+    case 1: {
+      y_start = 15;
+      vector<vec2i> p{
+          {fb.bounds.x - 6, y_start},     {fb.bounds.x - 5, y_start},
+          {fb.bounds.x - 4, y_start},     {fb.bounds.x - 3, y_start},
+          {fb.bounds.x - 2, y_start},     {fb.bounds.x - 1, y_start},
+          {fb.bounds.x, y_start},         {fb.bounds.x - 6, y_start + 1},
+          {fb.bounds.x - 5, y_start + 1}, {fb.bounds.x - 4, y_start + 1},
+          {fb.bounds.x - 3, y_start + 1}, {fb.bounds.x - 2, y_start + 1},
+          {fb.bounds.x - 1, y_start + 1}, {fb.bounds.x, y_start + 1},
+          {fb.bounds.x - 6, y_start + 2}, {fb.bounds.x - 5, y_start + 2},
+          {fb.bounds.x - 4, y_start + 2}, {fb.bounds.x - 3, y_start + 2},
+          {fb.bounds.x - 2, y_start + 2}, {fb.bounds.x - 1, y_start + 2},
+          {fb.bounds.x, y_start + 2},     {fb.bounds.x - 6, y_start + 3},
+          {fb.bounds.x - 5, y_start + 3}, {fb.bounds.x - 4, y_start + 3},
+          {fb.bounds.x - 3, y_start + 3}, {fb.bounds.x - 2, y_start + 3},
+          {fb.bounds.x - 1, y_start + 3}, {fb.bounds.x, y_start + 3},
+          {fb.bounds.x - 6, y_start + 4}, {fb.bounds.x - 5, y_start + 4},
+          {fb.bounds.x - 4, y_start + 4}, {fb.bounds.x - 3, y_start + 4},
+          {fb.bounds.x - 2, y_start + 4}, {fb.bounds.x - 1, y_start + 4},
+          {fb.bounds.x, y_start + 4}};
+
+      dc = " _   _ "
+           "| |_| |"
+           "|_   _|"
+           "  | |  "
+           "__|_|__";
+
+      Cactus obj1{p, dc, 1};
+      m_object_set.push_back(obj1);
+      break;
+    }
+
+    case 2: {
+      y_start = 13;
+      vector<vec2i> p{
+          {fb.bounds.x - 10, y_start},     {fb.bounds.x - 9, y_start},
+          {fb.bounds.x - 8, y_start},      {fb.bounds.x - 7, y_start},
+          {fb.bounds.x - 6, y_start},      {fb.bounds.x - 5, y_start},
+          {fb.bounds.x - 4, y_start},      {fb.bounds.x - 3, y_start},
+          {fb.bounds.x - 2, y_start},      {fb.bounds.x - 1, y_start},
+          {fb.bounds.x, y_start},          {fb.bounds.x - 10, y_start + 1},
+          {fb.bounds.x - 9, y_start + 1},  {fb.bounds.x - 8, y_start + 1},
+          {fb.bounds.x - 7, y_start + 1},  {fb.bounds.x - 6, y_start + 1},
+          {fb.bounds.x - 5, y_start + 1},  {fb.bounds.x - 4, y_start + 1},
+          {fb.bounds.x - 3, y_start + 1},  {fb.bounds.x - 2, y_start + 1},
+          {fb.bounds.x - 1, y_start + 1},  {fb.bounds.x, y_start + 1},
+          {fb.bounds.x - 10, y_start + 2}, {fb.bounds.x - 9, y_start + 2},
+          {fb.bounds.x - 8, y_start + 2},  {fb.bounds.x - 7, y_start + 2},
+          {fb.bounds.x - 6, y_start + 2},  {fb.bounds.x - 5, y_start + 2},
+          {fb.bounds.x - 4, y_start + 2},  {fb.bounds.x - 3, y_start + 2},
+          {fb.bounds.x - 2, y_start + 2},  {fb.bounds.x - 1, y_start + 2},
+          {fb.bounds.x, y_start + 2},      {fb.bounds.x - 10, y_start + 3},
+          {fb.bounds.x - 9, y_start + 3},  {fb.bounds.x - 8, y_start + 3},
+          {fb.bounds.x - 7, y_start + 3},  {fb.bounds.x - 6, y_start + 3},
+          {fb.bounds.x - 5, y_start + 3},  {fb.bounds.x - 4, y_start + 3},
+          {fb.bounds.x - 3, y_start + 3},  {fb.bounds.x - 2, y_start + 3},
+          {fb.bounds.x - 1, y_start + 3},  {fb.bounds.x, y_start + 3},
+          {fb.bounds.x - 10, y_start + 4}, {fb.bounds.x - 9, y_start + 4},
+          {fb.bounds.x - 8, y_start + 4},  {fb.bounds.x - 7, y_start + 4},
+          {fb.bounds.x - 6, y_start + 4},  {fb.bounds.x - 5, y_start + 4},
+          {fb.bounds.x - 4, y_start + 4},  {fb.bounds.x - 3, y_start + 4},
+          {fb.bounds.x - 2, y_start + 4},  {fb.bounds.x - 1, y_start + 4},
+          {fb.bounds.x, y_start + 4},      {fb.bounds.x - 10, y_start + 5},
+          {fb.bounds.x - 9, y_start + 5},  {fb.bounds.x - 8, y_start + 5},
+          {fb.bounds.x - 7, y_start + 5},  {fb.bounds.x - 6, y_start + 5},
+          {fb.bounds.x - 5, y_start + 5},  {fb.bounds.x - 4, y_start + 5},
+          {fb.bounds.x - 3, y_start + 5},  {fb.bounds.x - 2, y_start + 5},
+          {fb.bounds.x - 1, y_start + 5},  {fb.bounds.x, y_start + 5},
+          {fb.bounds.x - 10, y_start + 6}, {fb.bounds.x - 9, y_start + 6},
+          {fb.bounds.x - 8, y_start + 6},  {fb.bounds.x - 7, y_start + 6},
+          {fb.bounds.x - 6, y_start + 6},  {fb.bounds.x - 5, y_start + 6},
+          {fb.bounds.x - 4, y_start + 6},  {fb.bounds.x - 3, y_start + 6},
+          {fb.bounds.x - 2, y_start + 6},  {fb.bounds.x - 1, y_start + 6},
+          {fb.bounds.x, y_start + 6}};
+
+      dc = "     _     "
+           "    | |    "
+           " _  | |  _ "
+           "| |_| |_| |"
+           "|___   ___|"
+           "    | |    "
+           "____|_|____";
+
+      Cactus obj2{p, dc, 2};
+      m_object_set.push_back(obj2);
+      break;
+    }
+
+    case 3: {
+      y_start = 16;
+      vector<vec2i> p{
+          {fb.bounds.x - 10, y_start},     {fb.bounds.x - 9, y_start},
+          {fb.bounds.x - 8, y_start},      {fb.bounds.x - 7, y_start},
+          {fb.bounds.x - 6, y_start},      {fb.bounds.x - 5, y_start},
+          {fb.bounds.x - 4, y_start},      {fb.bounds.x - 3, y_start},
+          {fb.bounds.x - 2, y_start},      {fb.bounds.x - 1, y_start},
+          {fb.bounds.x, y_start},          {fb.bounds.x - 10, y_start + 1},
+          {fb.bounds.x - 9, y_start + 1},  {fb.bounds.x - 8, y_start + 1},
+          {fb.bounds.x - 7, y_start + 1},  {fb.bounds.x - 6, y_start + 1},
+          {fb.bounds.x - 5, y_start + 1},  {fb.bounds.x - 4, y_start + 1},
+          {fb.bounds.x - 3, y_start + 1},  {fb.bounds.x - 2, y_start + 1},
+          {fb.bounds.x - 1, y_start + 1},  {fb.bounds.x, y_start + 1},
+          {fb.bounds.x - 10, y_start + 2}, {fb.bounds.x - 9, y_start + 2},
+          {fb.bounds.x - 8, y_start + 2},  {fb.bounds.x - 7, y_start + 2},
+          {fb.bounds.x - 6, y_start + 2},  {fb.bounds.x - 5, y_start + 2},
+          {fb.bounds.x - 4, y_start + 2},  {fb.bounds.x - 3, y_start + 2},
+          {fb.bounds.x - 2, y_start + 2},  {fb.bounds.x - 1, y_start + 2},
+          {fb.bounds.x, y_start + 2},      {fb.bounds.x - 10, y_start + 3},
+          {fb.bounds.x - 9, y_start + 3},  {fb.bounds.x - 8, y_start + 3},
+          {fb.bounds.x - 7, y_start + 3},  {fb.bounds.x - 6, y_start + 3},
+          {fb.bounds.x - 5, y_start + 3},  {fb.bounds.x - 4, y_start + 3},
+          {fb.bounds.x - 3, y_start + 3},  {fb.bounds.x - 2, y_start + 3},
+          {fb.bounds.x - 1, y_start + 3},  {fb.bounds.x, y_start + 3}};
+
+      dc = "     _     "
+           " _  | |  _ "
+           "| |_| |_| |"
+           "|_________|";
+
+      Cactus obj3{p, dc, 3};
+      m_object_set.push_back(obj3);
+      break;
+    }
+
+    case 4: {
+      y_start = 14;
+      vector<vec2i> p{
+          {fb.bounds.x - 6, y_start},     {fb.bounds.x - 5, y_start},
+          {fb.bounds.x - 4, y_start},     {fb.bounds.x - 3, y_start},
+          {fb.bounds.x - 2, y_start},     {fb.bounds.x - 1, y_start},
+          {fb.bounds.x, y_start},         {fb.bounds.x - 6, y_start + 1},
+          {fb.bounds.x - 5, y_start + 1}, {fb.bounds.x - 4, y_start + 1},
+          {fb.bounds.x - 3, y_start + 1}, {fb.bounds.x - 2, y_start + 1},
+          {fb.bounds.x - 1, y_start + 1}, {fb.bounds.x, y_start + 1},
+          {fb.bounds.x - 6, y_start + 2}, {fb.bounds.x - 5, y_start + 2},
+          {fb.bounds.x - 4, y_start + 2}, {fb.bounds.x - 3, y_start + 2},
+          {fb.bounds.x - 2, y_start + 2}, {fb.bounds.x - 1, y_start + 2},
+          {fb.bounds.x, y_start + 2},     {fb.bounds.x - 6, y_start + 3},
+          {fb.bounds.x - 5, y_start + 3}, {fb.bounds.x - 4, y_start + 3},
+          {fb.bounds.x - 3, y_start + 3}, {fb.bounds.x - 2, y_start + 3},
+          {fb.bounds.x - 1, y_start + 3}, {fb.bounds.x, y_start + 3},
+          {fb.bounds.x - 6, y_start + 4}, {fb.bounds.x - 5, y_start + 4},
+          {fb.bounds.x - 4, y_start + 4}, {fb.bounds.x - 3, y_start + 4},
+          {fb.bounds.x - 2, y_start + 4}, {fb.bounds.x - 1, y_start + 4},
+          {fb.bounds.x, y_start + 4},     {fb.bounds.x - 6, y_start + 5},
+          {fb.bounds.x - 5, y_start + 5}, {fb.bounds.x - 4, y_start + 5},
+          {fb.bounds.x - 3, y_start + 5}, {fb.bounds.x - 2, y_start + 5},
+          {fb.bounds.x - 1, y_start + 5}, {fb.bounds.x, y_start + 5}};
+
+      dc = " _   _ "
+           "| | | |"
+           "| |_| |"
+           "|_   _|"
+           "  | |  "
+           "__|_|__";
+
+      Cactus obj4{p, dc, 4};
+      m_object_set.push_back(obj4);
+      break;
+    }
+
+    case 5: {
+      y_start = 7;
+      vector<vec2i> p{
+          {fb.bounds.x - 8, y_start},     {fb.bounds.x - 7, y_start},
+          {fb.bounds.x - 6, y_start},     {fb.bounds.x - 5, y_start},
+          {fb.bounds.x - 4, y_start},     {fb.bounds.x - 3, y_start},
+          {fb.bounds.x - 2, y_start},     {fb.bounds.x - 1, y_start},
+          {fb.bounds.x, y_start},         {fb.bounds.x - 8, y_start + 1},
+          {fb.bounds.x - 7, y_start + 1}, {fb.bounds.x - 6, y_start + 1},
+          {fb.bounds.x - 5, y_start + 1}, {fb.bounds.x - 4, y_start + 1},
+          {fb.bounds.x - 3, y_start + 1}, {fb.bounds.x - 2, y_start + 1},
+          {fb.bounds.x - 1, y_start + 1}, {fb.bounds.x, y_start + 1},
+          {fb.bounds.x - 8, y_start + 2}, {fb.bounds.x - 7, y_start + 2},
+          {fb.bounds.x - 6, y_start + 2}, {fb.bounds.x - 5, y_start + 2},
+          {fb.bounds.x - 4, y_start + 2}, {fb.bounds.x - 3, y_start + 2},
+          {fb.bounds.x - 2, y_start + 2}, {fb.bounds.x - 1, y_start + 2},
+          {fb.bounds.x, y_start + 2},     {fb.bounds.x - 8, y_start + 3},
+          {fb.bounds.x - 7, y_start + 3}, {fb.bounds.x - 6, y_start + 3},
+          {fb.bounds.x - 5, y_start + 3}, {fb.bounds.x - 4, y_start + 3},
+          {fb.bounds.x - 3, y_start + 3}, {fb.bounds.x - 2, y_start + 3},
+          {fb.bounds.x - 1, y_start + 3}, {fb.bounds.x, y_start + 3},
+          {fb.bounds.x - 8, y_start + 4}, {fb.bounds.x - 7, y_start + 4},
+          {fb.bounds.x - 6, y_start + 4}, {fb.bounds.x - 5, y_start + 4},
+          {fb.bounds.x - 4, y_start + 4}, {fb.bounds.x - 3, y_start + 4},
+          {fb.bounds.x - 2, y_start + 4}, {fb.bounds.x - 1, y_start + 4},
+          {fb.bounds.x, y_start + 4},     {fb.bounds.x - 8, y_start + 5},
+          {fb.bounds.x - 7, y_start + 5}, {fb.bounds.x - 6, y_start + 5},
+          {fb.bounds.x - 5, y_start + 5}, {fb.bounds.x - 4, y_start + 5},
+          {fb.bounds.x - 3, y_start + 5}, {fb.bounds.x - 2, y_start + 5},
+          {fb.bounds.x - 1, y_start + 5}, {fb.bounds.x, y_start + 5},
+          {fb.bounds.x - 8, y_start + 6}, {fb.bounds.x - 7, y_start + 6},
+          {fb.bounds.x - 6, y_start + 6}, {fb.bounds.x - 5, y_start + 6},
+          {fb.bounds.x - 4, y_start + 6}, {fb.bounds.x - 3, y_start + 6},
+          {fb.bounds.x - 2, y_start + 6}, {fb.bounds.x - 1, y_start + 6},
+          {fb.bounds.x, y_start + 6},
+      };
+
+      dc = "         "
+           "         "
+           "  _____  "
+           ">|_   _\\="
+           "   | /   "
+           "   |/    "
+           "   '     ";
+
+      Cactus obj5{p, dc, 5};
+      m_object_set.push_back(obj5);
+      break;
+    }
+
+    case 6: {
+      y_start = 13;
+      vector<vec2i> p{
+          {fb.bounds.x - 8, y_start},     {fb.bounds.x - 7, y_start},
+          {fb.bounds.x - 6, y_start},     {fb.bounds.x - 5, y_start},
+          {fb.bounds.x - 4, y_start},     {fb.bounds.x - 3, y_start},
+          {fb.bounds.x - 2, y_start},     {fb.bounds.x - 1, y_start},
+          {fb.bounds.x, y_start},         {fb.bounds.x - 8, y_start + 1},
+          {fb.bounds.x - 7, y_start + 1}, {fb.bounds.x - 6, y_start + 1},
+          {fb.bounds.x - 5, y_start + 1}, {fb.bounds.x - 4, y_start + 1},
+          {fb.bounds.x - 3, y_start + 1}, {fb.bounds.x - 2, y_start + 1},
+          {fb.bounds.x - 1, y_start + 1}, {fb.bounds.x, y_start + 1},
+          {fb.bounds.x - 8, y_start + 2}, {fb.bounds.x - 7, y_start + 2},
+          {fb.bounds.x - 6, y_start + 2}, {fb.bounds.x - 5, y_start + 2},
+          {fb.bounds.x - 4, y_start + 2}, {fb.bounds.x - 3, y_start + 2},
+          {fb.bounds.x - 2, y_start + 2}, {fb.bounds.x - 1, y_start + 2},
+          {fb.bounds.x, y_start + 2},     {fb.bounds.x - 8, y_start + 3},
+          {fb.bounds.x - 7, y_start + 3}, {fb.bounds.x - 6, y_start + 3},
+          {fb.bounds.x - 5, y_start + 3}, {fb.bounds.x - 4, y_start + 3},
+          {fb.bounds.x - 3, y_start + 3}, {fb.bounds.x - 2, y_start + 3},
+          {fb.bounds.x - 1, y_start + 3}, {fb.bounds.x, y_start + 3},
+          {fb.bounds.x - 8, y_start + 4}, {fb.bounds.x - 7, y_start + 4},
+          {fb.bounds.x - 6, y_start + 4}, {fb.bounds.x - 5, y_start + 4},
+          {fb.bounds.x - 4, y_start + 4}, {fb.bounds.x - 3, y_start + 4},
+          {fb.bounds.x - 2, y_start + 4}, {fb.bounds.x - 1, y_start + 4},
+          {fb.bounds.x, y_start + 4},     {fb.bounds.x - 8, y_start + 5},
+          {fb.bounds.x - 7, y_start + 5}, {fb.bounds.x - 6, y_start + 5},
+          {fb.bounds.x - 5, y_start + 5}, {fb.bounds.x - 4, y_start + 5},
+          {fb.bounds.x - 3, y_start + 5}, {fb.bounds.x - 2, y_start + 5},
+          {fb.bounds.x - 1, y_start + 5}, {fb.bounds.x, y_start + 5},
+          {fb.bounds.x - 8, y_start + 6}, {fb.bounds.x - 7, y_start + 6},
+          {fb.bounds.x - 6, y_start + 6}, {fb.bounds.x - 5, y_start + 6},
+          {fb.bounds.x - 4, y_start + 6}, {fb.bounds.x - 3, y_start + 6},
+          {fb.bounds.x - 2, y_start + 6}, {fb.bounds.x - 1, y_start + 6},
+          {fb.bounds.x, y_start + 6},
+      };
+
+      dc = "         "
+           "         "
+           "  _____  "
+           ">|_   _\\="
+           "   | /   "
+           "   |/    "
+           "___'_____";
+
+      Cactus obj6{p, dc, 6};
+      m_object_set.push_back(obj6);
+      break;
+    }
+    }
+  }
+
+  return false;
 }
